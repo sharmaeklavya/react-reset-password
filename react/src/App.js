7104cfb7db889d459965ef3d5f06bd7a71da36b8
eklavyasharma <24450132+sharmaeklavya@users.noreply.github.com>
2021-06-17 15:24:41 +0530

    bug fixes

--- react/src/App.js
+++ react/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { CSSTransition, SwitchTransition } from "react-transition-group";
 import { UserProvider } from "./UserContext";
 import ForgotPass from "./components/ForgotPass";
 import ResetPassword from "./components/ResetPass";
@@ -14,36 +13,14 @@ function App() {
     <Router>
       <UserProvider>
         <div className="container px-0">
-          <Route path="/dashboard" component={Dashboard}></Route>
-          <Route
-            render={({ location }) => (
-              <SwitchTransition>
-                <CSSTransition
-                  key={location.key}
-                  timeout={300}
-                  classNames="fadeLeft"
-                  mountOnEnter={true}
-                  unmountOnExit={true}
-                >
-                  <Switch location={location}>
-                    <Route exact path="/" component={UserLogin}></Route>
-                    <Route
-                      exact
-                      path="/forgotpassword"
-                      component={ForgotPass}
-                    ></Route>
-                    <Route path="/reset/:id" component={ResetPassword}></Route>
-                    <Route
-                      exact
-                      path="/register"
-                      component={RegisterUser}
-                    ></Route>
-                    <Route path="*" component={NotFound}></Route>
-                  </Switch>
-                </CSSTransition>
-              </SwitchTransition>
-            )}
-          />
+          <Switch>
+            <Route exact path="/dashboard" component={Dashboard}></Route>
+            <Route exact path="/forgotpassword" component={ForgotPass}></Route>
+            <Route exact path="/reset/:id" component={ResetPassword}></Route>
+            <Route exact path="/register" component={RegisterUser}></Route>
+            <Route exact path="/" component={UserLogin}></Route>
+            <Route path="*" component={NotFound}></Route>
+          </Switch>
         </div>
       </UserProvider>
     </Router>
